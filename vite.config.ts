fe97bd5fa91ef2627ca30fe960de89eef183613f
eythorsson <steinar@wise.no>
2024-06-25 21:55:17 +0200

    Don't bundle dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import pkg from './package.json' assert { type: 'json' }
 
 export default defineConfig({
     plugins: [
@@ -18,6 +19,12 @@ export default defineConfig({
             formats: ['es'],
             name: "common-utils",
             fileName: (format) => `index.${format}.js`,
+        },
+        rollupOptions: {
+            external: [
+                ...Object.keys(pkg.dependencies), // don't bundle dependencies
+                /^node:.*/, // don't bundle built-in Node.js modules (use protocol imports!)
+            ],
         },
     }
 })
