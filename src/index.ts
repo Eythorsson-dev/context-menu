a0ae1201152e5b184463243e3457c673491e9d32
eythorsson <steinar@wise.no>
2024-06-26 17:20:08 +0200

    Revert "bundle css + rebuild"
    This reverts commit efa176a7c4f8300101477ae7ec201cd3fa3c75d2.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { ContextMenuItem, type ContextMenuItemProps } from "./contextMenuItem";
 import { ContextMenuOption, type ContextMenuOptionConfig } from "./contextMenuOption";
 import { ContextMenuOptionGroup, type ContextMenuOptionGroupConfig } from "./contextMenuOptionGroup";
 
-import "../index.css"
+
 
 export {
     ContextMenu,
